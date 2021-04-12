35fd71433f1530d944c61231f4f5ea9848906286
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-12 19:22:27 +0800

    update index.php

--- index.php
+++ index.php
@@ -11,7 +11,14 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    
+    <form action="" method="get">
+      <select name="kota" class="form-select mb-3">
+        <?php foreach ($kota as $row): ?>
+          
+        <?php endforeach; ?>
+      </select>
+    </form>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
   </body>
