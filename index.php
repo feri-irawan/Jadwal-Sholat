4ed0f301a0ed97e4a56b2b739583171383abaa68
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-13 06:02:51 +0800

    update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ $kota = file_get_contents("https://api.myquran.com/v1/sholat/kota/semua");
 $kota = json_decode($kota, true);
 if (isset($_GET["kota"])) {
   $url = urldecode($_GET["kota"]);
-  $replace_from = array("[", "{", "\"", ":", "}", "]");
-  $replace_to = array("","","","","","");
+  $replace_from = array("[");
+  $replace_to = "";
   $namakota = str_replace($replace_from, $replace_to, $url);
   
   $cari = file_get_contents("https://api.myquran.com/v1/sholat/kota/cari/kediri");
