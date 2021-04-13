8080bd355e424eeee1ef79a56d219aaa62900bba
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-13 15:18:33 +0800

    update index.php

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ $sholatIcon = "https://static.thenounproject.com/png/3358992-200.png";
       </nav>
     </header>
     <!-- Modal -->
-    <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
