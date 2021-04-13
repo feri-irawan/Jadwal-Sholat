47c30a311f55e4359e5e4af848373e91ce6a0d58
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-13 10:58:31 +0800

    update index.php

--- index.php
+++ index.php
@@ -73,19 +73,13 @@ $jadwal = json_decode($jadwal, true);
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 0c-3.148 0-6 2.553-6 5.702 0 4.682 4.783 5.177 6 12.298 1.217-7.121 6-7.616 6-12.298 0-3.149-2.851-5.702-6-5.702zm0 8c-1.105 0-2-.895-2-2s.895-2 2-2 2 .895 2 2-.895 2-2 2zm12 16l-6.707-2.427-5.293 2.427-5.581-2.427-6.419 2.427 4-9 3.96-1.584c.38.516.741 1.08 1.061 1.729l-3.523 1.41-1.725 3.88 2.672-1.01 1.506-2.687-.635 3.044 4.189 1.789.495-2.021.465 2.024 4.15-1.89-.618-3.033 1.572 2.896 2.732.989-1.739-3.978-3.581-1.415c.319-.65.681-1.215 1.062-1.731l4.021 1.588 3.936 9z"/></svg>
             </label>
             <input id="kota" name="kota" type="text" class="form-control" placeholder="Kota..." aria-describedby="basic-addon1">
-          </div>
-          
-        </div>
-        <div class="col">
-          <div class="input-group mb-3">
-            <label for="timezone" class="input-group-text">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm5.848 12.459c.202.038.202.333.001.372-1.907.361-6.045 1.111-6.547 1.111-.719 0-1.301-.582-1.301-1.301 0-.512.77-5.447 1.125-7.445.034-.192.312-.181.343.014l.985 6.238 5.394 1.011z"/></svg>
-            </label>
             <select id="timezone" name="timezone" class="form-select">
               <option value="Jakarta">WIB</option>
               <option value="Makassar">WITA</option>
               <option value="Jayapura">WIT</option>
             </select>
+          </div>
+          
           </div>
         </div>
       </div>
