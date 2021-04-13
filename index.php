fde11ec6c20b35be7e5339bdb69ae422dd42a97d
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-13 17:00:55 +0800

    update index.php

--- index.php
+++ index.php
@@ -220,7 +220,7 @@ $sholatIcon = "https://static.thenounproject.com/png/3358992-200.png";
               <div class="col">
                 <b>Maghrib <img src="<?=$sholatIcon?>" width="18" height="18"> <br />
                 Buka Puasa</b>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUMxW3Rbu1r4XDgMyh83KrxjoLcW7OaU2cTw&usqp=CAU" width="16" height="16">
+                <img src="https://www.clipartmax.com/png/full/241-2415208_eat-comments-eat-png-free-icon.png" width="16" height="16">
               </div>
               <div class="col text-end">
                 <?=$jadwal["data"]["jadwal"]["maghrib"]?>
