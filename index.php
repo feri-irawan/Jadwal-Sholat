3261c4905efb85eea966a6e562951dbecfa33f71
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-13 11:04:48 +0800

    update index.php

--- index.php
+++ index.php
@@ -189,14 +189,6 @@ $jadwal = json_decode($jadwal, true);
         </div>
       </div>
     </div>
-    
-    <pre>
-      <code>
-        <?php
-        print_r($jadwal);
-        ?>
-      </code>
-    </pre>
   </section>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
