e6ff3d47d5f2a1b82bd3e90758b31e83294c7c98
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-13 15:48:50 +0800

    update index.php

--- index.php
+++ index.php
@@ -269,6 +269,28 @@ $sholatIcon = "https://static.thenounproject.com/png/3358992-200.png";
       var myModal = new bootstrap.Modal(document.getElementById("staticBackdrop"), {});
       myModal.show();
     }
+    
+    
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
     </script>
   </body>
 </html>
