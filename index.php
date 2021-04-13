6d7595d52fd79f102f435733dc0c8ae38ea71f81
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-13 10:24:31 +0800

    update index.php

--- index.php
+++ index.php
@@ -33,14 +33,20 @@ $jadwal = json_decode($jadwal, true);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
+    <!-- CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/css/ol.css" type="text/css">
+    <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/build/ol.js"></script>
+
     <title>Jadwal Sholat</title>
-  
     <style>
       #jadwal b {
         text-transform: uppercase;
         font-size: 14px;
+      }
+      .map {
+        height: 400px;
+        width: 100%;
       }
     </style>
   </head>
@@ -188,5 +194,24 @@ $jadwal = json_decode($jadwal, true);
   </section>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+    window.addEventListener('load', function () {
+      function getMap(latitude, longitude) {
+        var map = new ol.Map({
+          target: 'map',
+          controls: ol.control.defaults({ attribution: false }),
+          layers: [
+            new ol.layer.Tile({
+              source: new ol.source.OSM()
+            })
+          ],
+          view: new ol.View({
+            center: ol.proj.fromLonLat([longitude, latitude]),
+            zoom: 12
+          })
+        });
+      }
+    }, false); 
+    </script>
   </body>
 </html>
