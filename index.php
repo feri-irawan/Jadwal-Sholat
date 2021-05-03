17b80bdb6b9767ebe9df0ee0666e4831f113710f
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-05-04 03:42:43 +0800

    update index.php

--- index.php
+++ index.php
@@ -118,7 +118,7 @@ $sholatIcon = "https://static.thenounproject.com/png/3358992-200.png";
         
         <div class="row">
           <div class="col-8">
-            Sekarang Pukul : <span id="time"></span>
+            Sekarang pukul : <span id="time"></span>
           </div>
           <div class="col-4">
             <div class="text-end">
